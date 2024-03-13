4f465bb2a81b4b5f04af5ed0231cf3c585cb2257
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-03-13 20:45:37 +0100

    Checks important age

---
+++ agecheck.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+age = eval(input("Enter age: "))
+if (age >= 1) and (age <= 18):
+    print("Important Birthday")
+elif (age == 21) or (age == 50):
+    print("Important Birthday")
+elif not(age < 65):
+    print("Important Birthday")
+else:
+    print("Not Important Birthday")

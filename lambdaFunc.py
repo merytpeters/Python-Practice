c303cb2598b618ded47226cf0a939c23edc342a0
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-03-20 20:41:21 +0100

    addition using lambda and function definition

---
+++ lambdaFunc.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+sum = lambda x, y : x + y
+result = sum(3,4)
+print(result)
---
+++ sum.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def sum(x,y):
+    return x + y
+result = sum(3,4)
+print(result)
